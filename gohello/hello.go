88e818b32e6e95aa8cd3dab7a6af77a52c8a489f
Chris Sumption <fridaysedge@gmail.com>
2016-02-22 10:03:38 -0800

    go lecture

---
+++ gohello/hello.go
@@ -0,0 +1,13 @@
+package main 
+
+import (
+	"fmt"
+	"os"
+	"github.com/fridaysedge/info344-class-code/gohello/reverse"
+)
+
+func main(){
+	fmt.Println(reverse.Reverse("Hello World"))
+	fmt.Println(os.Args[0])
+}
+
