cfd8264c83d4575a06e454a9db9d19f998111073
Chris Sumption <fridaysedge@gmail.com>
2016-02-29 09:40:07 -0800

    add comments

--- gowebserver/gowebserver.go
+++ gowebserver/gowebserver.go
@@ -9,26 +9,33 @@ import (
 )
 
 type HelloResponse struct {
+	// Override the json output using`` syntax after variables/types
 	Name string `json:"name"`
 	Message string `json:"message"`
 	GeneratedAt time.Time `json:"generatedAt"`
 }
 
 func sayHello(w http.ResponseWriter, r *http.Request){
+	// Get whatever follows /api/v1/hello/
+	// On the requested URL
 	name := r.URL.Path[len("/api/v1/hello/"):]
+	// Create and initialize the response structure
 	resp := HelloResponse{Name: name, 
 		Message: "Hello " + name,
 		GeneratedAt: time.Now()}
 	
+	// Convert the structure to json
 	j, err := json.Marshal(resp)
 	if nil != err {
 		log.Println(err)
 		w.WriteHeader(500)
 		w.Write([]byte(err.Error()))
 	} else {
+		// Tell the client we are sending back JSON
 		w.Header().Add("Content-Type", "application/json")
 		w.Write(j)
 	}
+	// Basic examples
 	//w.Write([]byte("Hello World"))
 	//w.Write([]byte("Hello " + name))
 }
@@ -40,6 +47,8 @@ func main(){
 	http.HandleFunc("/api/v1/hello/", sayHello)
 	
 	fmt.Println("Server listening on port 9000")
+	
+	// This stops the flow until the server is manually shut down
 	http.ListenAndServe(":9000", nil)
 }
 
